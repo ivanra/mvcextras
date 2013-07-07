eb4c5aaa5a89a59efe30c19ec99b8b4e43a95d25
Ivan Radanovic <ivanra@gmail.com>
2013-07-07 09:56:46 +0200

    Add StrictTransportSecurityAttribute.

---
+++ src/MvcExtras.Test/StrictTransportSecurityAttributeTest.cs
@@ -0,0 +1,43 @@
+﻿using Moq;
+using System;
+using System.Web.Mvc;
+using Xunit;
+using Xunit.Extensions;
+
+namespace MvcExtras.Test
+{
+    public class StrictTransportSecurityAttributeTest
+    {
+
+        [Fact]
+        public void ThrowsIfFilterContextNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => new StrictTransportSecurityAttribute().OnResultExecuting(null));
+        }
+
+        [Theory]
+        [InlineData(null, null, "max-age=60")]
+        [InlineData(3600, null, "max-age=3600")]
+        [InlineData(null, true, "max-age=60; includeSubDomains")]
+        [InlineData(3600, true, "max-age=3600; includeSubDomains")]
+        public void DefaultMaxAge(int? maxAge, bool? includeSubdomains, string expectedHeaderValue)
+        {
+            var mockResultExecutingContext = new Mock<ResultExecutingContext>();
+            mockResultExecutingContext.Setup(
+                c => c.HttpContext.Response.AddHeader("Strict-Transport-Security", expectedHeaderValue))
+                                      .Verifiable("Invalid Strict-Transport-Security header.");
+            var resultExecutingContext = mockResultExecutingContext.Object;
+            
+            var attr = new StrictTransportSecurityAttribute();            
+            if (maxAge.HasValue)
+                attr.MaxAge = maxAge.Value;
+            if (includeSubdomains.HasValue)
+                attr.IncludeSubdomains = includeSubdomains.Value;
+
+            attr.OnResultExecuting(resultExecutingContext);
+
+            mockResultExecutingContext.Verify();
+        }
+
+    }
+}
---
+++ src/MvcExtras/StrictTransportSecurityAttribute.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace MvcExtras
+{
+    /// <summary>
+    /// StrictTransportSecurityAttribute -- render Strict-Transport-Security HTTP header on response (as per RFC 6797: http://tools.ietf.org/html/rfc6797)
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = false)]
+    public class StrictTransportSecurityAttribute : FilterAttribute, IResultFilter
+    {
+        private const string StrictTransportPolicyHeader = "Strict-Transport-Security";
+        private const int DefaultMaxAge = 60;
+
+        public StrictTransportSecurityAttribute()
+        {
+            MaxAge = DefaultMaxAge;
+        }
+
+        public int MaxAge { get; set; }
+
+        public bool IncludeSubdomains { get; set; }
+
+        public void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            if (filterContext == null)
+                throw new ArgumentNullException("filterContext");
+
+            //if (filterContext.HttpContext.Request.IsSecureConnection)
+            //    return;
+
+            var headerValue = String.Format("max-age={0}{1}", MaxAge,
+                                            IncludeSubdomains ? "; includeSubDomains" : String.Empty);
+            filterContext.HttpContext.Response.AddHeader(StrictTransportPolicyHeader, headerValue);
+        }
+
+        public void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+        }
+    }
+}
